686835657efd819c2e6862efd06e7b324e8881b5
James Dinsdale <hi@molovo.co>
2017-07-05 22:42:30 +0000

    Update homebrew formula

--- Formula/revolver.rb
+++ Formula/revolver.rb
@@ -1,16 +1,13 @@
 class Revolver < Formula
   desc "Progress spinner for ZSH scripts"
   homepage "https://github.com/molovo/revolver"
-  url "https://github.com/molovo/revolver/archive/v0.2.3.tar.gz"
-  sha256 "1bcd00126047e0faaacb496e6f01903efb5e5d3a26c72aa6f78061f08c0bf939"
-
+  url "https://github.com/molovo/revolver/archive/v0.2.4.tar.gz"
+  sha256 "14fc00f9afecf5fd9db639ce1a237115faf8d21c5168fbc5dc87a04717a42caa"
   bottle :unneeded
-
   def install
     bin.install "revolver"
     zsh_completion.install "revolver.zsh-completion" => "_revolver"
   end
-
   test do
     assert_equal version.to_s, shell_output("#{bin}/revolver -v")
   end
